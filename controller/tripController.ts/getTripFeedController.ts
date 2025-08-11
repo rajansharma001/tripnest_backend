149362a02da0b50e3c8dd76bc2acf8665c2189e0
Rajan Sharma <email.rajan001@gmail.com>
2025-08-11 04:18:31 +0300

    TripLast Trip & Feed

---
+++ controller/tripController.ts/getTripFeedController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { Trip } from "../../models/tirpScehma";
+
+export const GetTripFeed = async (req: Request, res: Response) => {
+  try {
+    const getTripFeed = await Trip.find({ visibility: true }).sort({
+      createdAt: -1,
+    });
+    if (!getTripFeed) {
+      return res.status(403).json({ error: "Trips not found." });
+    }
+    return res
+      .status(200)
+      .json({ success: "Trips fetched successfully.", getTripFeed });
+  } catch (error) {
+    console.log("Bad Request for fetching Trip Feed.");
+    return res
+      .status(500)
+      .json({ error: "Something went wrong while fetching feed." });
+  }
+};
+
+export const GetTripFeedById = async (req: Request, res: Response) => {
+  try {
+    const _id = req.params.id;
+    const getTripFeedById = await Trip.findOne({ _id, visibility: true });
+    if (!getTripFeedById) {
+      return res.status(403).json({ error: "Trip not found." });
+    }
+    return res
+      .status(200)
+      .json({ success: "Trips fetched By Id successfully.", getTripFeedById });
+  } catch (error) {
+    console.log("Bad Request for fetching Trip By Id.");
+    return res
+      .status(500)
+      .json({ error: "Something went wrong while fetching feed by Id." });
+  }
+};
--- routes/tripRoute.ts
+++ routes/tripRoute.ts
@@ -1,10 +1,15 @@
-import { Router } from "express";
 import express from "express";
 import { createTrip } from "../controller/tripController.ts/createTripController";
 import { verifyRole } from "../middleware/roles/verifyRoles";
 import { getLastTrip } from "../controller/tripController.ts/getLastTrip";
+import {
+  GetTripFeed,
+  GetTripFeedById,
+} from "../controller/tripController.ts/getTripFeedController";
 
 export const tripRoute = express.Router();
 
 tripRoute.post("/new-trip", verifyRole(["admin", "user"]), createTrip);
 tripRoute.get("/get-trip", verifyRole(["admin", "user"]), getLastTrip);
+tripRoute.get("/get-feed", GetTripFeed);
+tripRoute.get("/get-feed/:id", GetTripFeedById);
